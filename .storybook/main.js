735396723a253ca4d75a5833ea95b0d17c5b823e
ahmedosama7450 <a.osama7450@gmail.com>
2022-04-20 13:07:01 +0200

    Add tailwindcss

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,54 @@
+const path = require("path");
+
 module.exports = {
-  stories: [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  staticDirs: ["../src/assets"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    {
+      /**
+       * TODO: Can I just add the addon directly without configuration ?
+       * Fix Storybook issue with PostCSS@8
+       * @see https://github.com/storybookjs/storybook/issues/12668#issuecomment-773958085
+       */
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
   ],
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-webpack5",
   },
+  webpackFinal: (config) => {
+    /**
+     * TODO: Do I need this ?
+     * Add support for alias-imports
+     * @see https://github.com/storybookjs/storybook/issues/11989#issuecomment-715524391
+     */
+    config.resolve.alias = {
+      ...config.resolve?.alias,
+      "@": [path.resolve(__dirname, "../src/"), path.resolve(__dirname, "../")],
+    };
+
+    /**
+     * TODO: Do I need this ?
+     * Fixes font import with /
+     * @see https://github.com/storybookjs/storybook/issues/12844#issuecomment-867544160
+     */
+    config.resolve.roots = [
+      path.resolve(__dirname, "../public"),
+      "node_modules",
+    ];
+
+    return config;
+  },
+  // TODO: This solves webpack 5 issue with storybook and typescript. See https://stackoverflow.com/a/71031198
   typescript: {
     check: false,
     checkOptions: {},
